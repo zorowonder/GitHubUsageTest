0b783bb68505d32d3ce0eb0574c5a5942c68282d
biyongsen <708980827@qq.com>
2015-07-29 17:22:11 +0800

    add class
    add constructor
    
    add toString

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by zorowonder on 2015/7/29.
+ */
+public class GitTest {
+    public GitTest() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "GitTest{}";
+    }
+}
